78689ef199a78e368eb0c08ae7903f9eefae7089
andrekorol <anrobits@yahoo.com.br>
2020-08-10 17:23:23 -0300

    Unblock the event loop

--- server/password.js
+++ server/password.js
@@ -3,13 +3,15 @@ const _sodium = require("libsodium-wrappers");
 exports.seed = async () => {
   await _sodium.ready;
   const sodium = _sodium;
-  return sodium.randombytes_buf(sodium.crypto_pwhash_SALTBYTES);
+  const seed = await sodium.randombytes_buf(sodium.crypto_pwhash_SALTBYTES);
+
+  return seed;
 };
 
 exports.getHash = async (password, salt) => {
   await _sodium.ready;
   const sodium = _sodium;
-  return sodium.crypto_pwhash(
+  const hash = await sodium.crypto_pwhash(
     sodium.crypto_pwhash_BYTES_MIN,
     password,
     salt,
@@ -17,4 +19,6 @@ exports.getHash = async (password, salt) => {
     sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE,
     sodium.crypto_pwhash_ALG_DEFAULT
   );
+
+  return hash;
 };
