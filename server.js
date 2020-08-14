d35e6d4995b37c748325fdc7c77a10260b093803
andrekorol <anrobits@yahoo.com.br>
2020-08-14 00:09:54 -0300

    Send an Internal Server Error when a file cannot be served

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ app.use("/api/users", users);
 
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build/index.html"));
+  res.sendFile(path.join(__dirname, "build/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
 app.get("/admin/sitekey", (_, res) => {
