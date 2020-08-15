75cd21535e66c2c9b31dbfb9b20b88ba220ebf2b
andrekorol <anrobits@yahoo.com.br>
2020-08-14 23:08:02 -0300

    Move admin requests to their own router

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const path = require("path");
 const { initDB } = require("./src/server/database");
 const http = require("http");
 const history = require("connect-history-api-fallback");
-const { getSeed } = require("./src/server/password");
 const users = require("./src/api/users");
+const admin = require("./src/api/admin");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -19,6 +19,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(history());
 app.use("/api/users", users);
+app.use("/admin", admin);
 
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/", (req, res) => {
@@ -27,20 +28,6 @@ app.get("/", (req, res) => {
       res.status(500).send(err);
     }
   });
-});
-
-app.get("/admin/sitekey", (_, res) => {
-  res.status(200).send(process.env.HCAPTCHA_SITEKEY);
-});
-
-app.get("/admin/seed", (req, res) => {
-  getSeed()
-    .then((buf) => {
-      res.status(200).send(buf);
-    })
-    .catch(() => {
-      res.status(500).send("Error generating random seed");
-    });
 });
 
 const port = process.env.PORT || 5000;
---
+++ src/api/admin.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { getSeed } = require("../server/password");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const router = express.Router();
+
+router.get("/sitekey", (_, res) => {
+  res.status(200).send(process.env.HCAPTCHA_SITEKEY);
+});
+
+router.get("/seed", (_, res) => {
+  getSeed()
+    .then((buf) => {
+      res.status(200).send(buf);
+    })
+    .catch(() => {
+      res.status(500).send("Error generating random seed");
+    });
+});
+
+module.exports = router;
